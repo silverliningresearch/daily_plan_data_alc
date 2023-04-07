2085319f99cc21a392cdd1c940d6640e8a14516d
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-07 12:25:02 +0700

    update new data

---
+++ MUC/data/interview_data.js
@@ -0,0 +1,50 @@
+let interview_data_raw = `[
+    {
+        "InterviewId": "1",
+        "InterviewEndDate": "2023-04-03 10:06:22 GMT",
+        "InterviewState": "Complete",
+        "Flight": "X3 4282",
+        "Dest": "HER",
+        "AirlineCode": "X3",
+        "AirlineName": "TUI fly",
+        "download_time": "07-04-2023 12:25:02"
+    },
+    {
+        "InterviewId": "4",
+        "InterviewEndDate": "2023-04-07 01:10:01 GMT",
+        "InterviewState": "Complete",
+        "Flight": "BA807",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "AirlineName": "British Airways"
+    },
+    {
+        "InterviewId": "5",
+        "InterviewEndDate": "2023-04-07 04:31:53 GMT",
+        "InterviewState": "Complete",
+        "Flight": "LH843",
+        "Dest": "FRA",
+        "AirlineCode": "LH",
+        "AirlineName": "Lufthansa German Airlines"
+    },
+    {
+        "InterviewId": "6",
+        "InterviewEndDate": "2023-04-07 04:32:25 GMT",
+        "InterviewState": "Complete",
+        "Flight": "BA807",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "AirlineName": "British Airways"
+    },
+    {
+        "InterviewId": "7",
+        "InterviewEndDate": "2023-04-07 04:33:54 GMT",
+        "InterviewState": "Complete",
+        "Flight": "KL1340",
+        "Dest": "AMS",
+        "AirlineCode": "KL",
+        "AirlineName": "KLM Royal Dutch Airline"
+    }
+]    
+
+ `;
