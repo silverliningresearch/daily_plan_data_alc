7fa29232f55e96c3fe432875806c21addd870dfe
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-07 18:28:52 +0700

    update new data

---
+++ BLL_QSM_Dep/data/interview_data.js
@@ -0,0 +1,68 @@
+let interview_data_raw = `[
+    {
+        "InterviewId": "1",
+        "InterviewEndDate": "2023-04-03 10:06:22 GMT",
+        "InterviewState": "Complete",
+        "Flight": "X3 4282",
+        "Dest": "HER",
+        "AirlineCode": "X3",
+        "AirlineName": "TUI fly",
+        "download_time": "07-04-2023 13:28:52"
+    },
+    {
+        "InterviewId": "4",
+        "InterviewEndDate": "2023-04-07 01:10:01 GMT",
+        "InterviewState": "Complete",
+        "Flight": "BA807",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "AirlineName": "British Airways"
+    },
+    {
+        "InterviewId": "5",
+        "InterviewEndDate": "2023-04-07 04:31:53 GMT",
+        "InterviewState": "Complete",
+        "Flight": "LH843",
+        "Dest": "FRA",
+        "AirlineCode": "LH",
+        "AirlineName": "Lufthansa German Airlines"
+    },
+    {
+        "InterviewId": "6",
+        "InterviewEndDate": "2023-04-07 04:32:25 GMT",
+        "InterviewState": "Complete",
+        "Flight": "BA807",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "AirlineName": "British Airways"
+    },
+    {
+        "InterviewId": "7",
+        "InterviewEndDate": "2023-04-07 04:33:54 GMT",
+        "InterviewState": "Complete",
+        "Flight": "KL1340",
+        "Dest": "AMS",
+        "AirlineCode": "KL",
+        "AirlineName": "KLM Royal Dutch Airline"
+    },
+    {
+        "InterviewId": "8",
+        "InterviewEndDate": "2023-04-07 05:33:19 GMT",
+        "InterviewState": "Complete",
+        "Flight": "AF1763",
+        "Dest": "CDG",
+        "AirlineCode": "AF",
+        "AirlineName": "AIR FRANCE DENMARK c/o KLM FSSC"
+    },
+    {
+        "InterviewId": "9",
+        "InterviewEndDate": "2023-04-07 06:32:21 GMT",
+        "InterviewState": "Complete",
+        "Flight": "BA807",
+        "Dest": "LHR",
+        "AirlineCode": "BA",
+        "AirlineName": "British Airways"
+    }
+]    
+
+ `;
