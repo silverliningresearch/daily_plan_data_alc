80be6df6019a796269fb083d80dc6ab6e08aa88c
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-08 08:31:53 +0700

    update new data

--- BLL_QSM_Dep/data/interview_data.js
+++ BLL_QSM_Dep/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "HER",
         "AirlineCode": "X3",
         "AirlineName": "TUI fly",
-        "download_time": "07-04-2023 21:29:32"
+        "download_time": "08-04-2023 03:31:52"
     },
     {
         "InterviewId": "4",
--- MUC_PEI_AB/data/interview_data.js
+++ MUC_PEI_AB/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "HER",
         "AirlineCode": "X3",
         "AirlineName": "TUI fly",
-        "download_time": "07-04-2023 21:29:30"
+        "download_time": "08-04-2023 03:31:51"
     },
     {
         "InterviewId": "4",
