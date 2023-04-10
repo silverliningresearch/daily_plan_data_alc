8fd933fc577c42d6e3cdd0d650f21508fbbac695
lexuanbinh <binh.le.xuan@gmail.com>
2023-04-10 21:09:06 +0700

    update new data

--- BLL_QSM_Dep/data/interview_data_arr.js
+++ BLL_QSM_Dep/data/interview_data_arr.js
@@ -5,7 +5,7 @@ let interview_data_arr_raw = `[
         "InterviewState": "Complete",
         "Airline": "KL - KLM",
         "Dest": "AMS",
-        "download_time": "10-04-2023 14:07:38"
+        "download_time": "10-04-2023 16:09:02"
     },
     {
         "InterviewId": "2740",
--- BUD_QSM_Dep/data/interview_data.js
+++ BUD_QSM_Dep/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "10-04-2023 14:07:41"
+        "download_time": "10-04-2023 16:09:06"
     },
     {
         "InterviewId": "3965",
