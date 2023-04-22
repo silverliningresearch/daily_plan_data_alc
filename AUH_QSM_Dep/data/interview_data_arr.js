b70c574c047b279cf1a518970c899bbea987e12c
unknown <lb@silverliningresearch.com>
2023-04-22 01:35:28 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data_arr.js
+++ AUH_QSM_Dep/data/interview_data_arr.js
@@ -7,7 +7,7 @@ let interview_data_arr_raw = `[
         "Dest": "BOM",
         "AirlineCode": "EY",
         "Terminal": "T3",
-        "download_time": "22-04-2023 04:35:28"
+        "download_time": "22-04-2023 05:35:27"
     },
     {
         "InterviewId": "4480",
