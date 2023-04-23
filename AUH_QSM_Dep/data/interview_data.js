fe341a8cb07072ee8e706c25732aa739a37b9de9
unknown <lb@silverliningresearch.com>
2023-04-23 06:35:32 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data.js
+++ AUH_QSM_Dep/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "CAI",
         "AirlineCode": "MS915",
         "Terminal": "T1",
-        "download_time": "23-04-2023 09:35:30"
+        "download_time": "23-04-2023 10:35:29"
     },
     {
         "InterviewId": "7868",
