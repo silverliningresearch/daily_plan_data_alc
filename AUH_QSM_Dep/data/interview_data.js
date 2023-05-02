9d4ed1182d729eeccd44277ea18fc7080160ce63
unknown <lb@silverliningresearch.com>
2023-05-02 04:36:42 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data.js
+++ AUH_QSM_Dep/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "CAI",
         "AirlineCode": "MS915",
         "Terminal": "T1",
-        "download_time": "02-05-2023 07:36:36"
+        "download_time": "02-05-2023 08:36:35"
     },
     {
         "InterviewId": "7333",
--- MUC_PEI_AB/data/interview_data_arr.js
+++ MUC_PEI_AB/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "AA - American Airlines",
         "Dest": "CLT",
         "AirlineCode": "AA",
-        "download_time": "02-05-2023 05:35:01"
+        "download_time": "02-05-2023 06:34:59"
     },
     {
         "InterviewId": "5174",
