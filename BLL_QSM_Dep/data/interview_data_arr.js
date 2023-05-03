c545c6a0057f3bdbea61534377acb6ae87fb79f9
unknown <lb@silverliningresearch.com>
2023-05-03 01:36:48 +0000

    update new data

--- BLL_QSM_Dep/data/interview_data_arr.js
+++ BLL_QSM_Dep/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "KL - KLM",
         "Dest": "AMS",
         "AirlineCode": "KL",
-        "download_time": "03-05-2023 02:35:38"
+        "download_time": "03-05-2023 03:35:40"
     },
     {
         "InterviewId": "2077",
--- MUC_PEI_AB/data/interview_data_arr.js
+++ MUC_PEI_AB/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "AA - American Airlines",
         "Dest": "CLT",
         "AirlineCode": "AA",
-        "download_time": "03-05-2023 02:35:01"
+        "download_time": "03-05-2023 03:35:03"
     },
     {
         "InterviewId": "5174",
