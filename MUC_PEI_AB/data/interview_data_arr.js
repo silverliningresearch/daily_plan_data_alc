c8d9c39cd7a8ca06b801ee89adace2a7f896ac27
unknown <lb@silverliningresearch.com>
2023-05-08 14:37:13 +0000

    update new data

--- MUC_PEI_AB/data/interview_data_arr.js
+++ MUC_PEI_AB/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "AA - American Airlines",
         "Dest": "CLT",
         "AirlineCode": "AA",
-        "download_time": "08-05-2023 15:35:06"
+        "download_time": "08-05-2023 16:35:08"
     },
     {
         "InterviewId": "5174",
