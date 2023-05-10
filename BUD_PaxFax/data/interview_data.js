0d40d67422675de421a3f5aaa35dc982ec4ea236
unknown <lb@silverliningresearch.com>
2023-05-10 02:36:56 +0000

    update new data

--- BUD_PaxFax/data/interview_data.js
+++ BUD_PaxFax/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "W6 2335",
         "Dest": "MXP",
         "AirlineCode": "W6",
-        "download_time": "10-05-2023 03:36:32"
+        "download_time": "10-05-2023 04:36:33"
     },
     {
         "InterviewId": "7701",
--- BUD_Persona/data/interview_data.js
+++ BUD_Persona/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "10-05-2023 03:36:34"
+        "download_time": "10-05-2023 04:36:36"
     },
     {
         "InterviewId": "2039",
