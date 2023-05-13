4884106caeee6a633e9560f8a001c6f3adbe7d15
unknown <lb@silverliningresearch.com>
2023-05-13 01:37:05 +0000

    update new data

--- BUD_QSM_Dep/data/interview_data.js
+++ BUD_QSM_Dep/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "13-05-2023 02:35:53"
+        "download_time": "13-05-2023 03:35:53"
     },
     {
         "InterviewId": "3965",
--- BUD_QSM_Dep/data/interview_data_arr.js
+++ BUD_QSM_Dep/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "W6 - Wizzair",
         "Dest": "TLV",
         "AirlineCode": "W6",
-        "download_time": "13-05-2023 02:35:55"
+        "download_time": "13-05-2023 03:35:56"
     },
     {
         "InterviewId": "1224",
