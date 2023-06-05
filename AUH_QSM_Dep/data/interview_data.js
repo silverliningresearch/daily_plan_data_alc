d06e137d2abc1ff429c865b33b17e6781b2188f4
unknown <lb@silverliningresearch.com>
2023-06-05 01:30:36 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data.js
+++ AUH_QSM_Dep/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "CAI",
         "AirlineCode": "MS915",
         "Terminal": "T1",
-        "download_time": "05-06-2023 04:30:25"
+        "download_time": "05-06-2023 05:30:26"
     },
     {
         "InterviewId": "7333",
--- BUD_Route/data/interview_data.js
+++ BUD_Route/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "W6 2297",
         "Dest": "RUH",
         "AirlineCode": "W6",
-        "download_time": "05-06-2023 02:29:51"
+        "download_time": "05-06-2023 03:29:49"
     },
     {
         "InterviewId": "67210",
