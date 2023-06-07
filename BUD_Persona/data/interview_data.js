faf8eb36a6759be464d4f4bcccceac90c36698e3
unknown <lb@silverliningresearch.com>
2023-06-07 03:30:52 +0000

    update new data

--- BUD_Persona/data/interview_data.js
+++ BUD_Persona/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "07-06-2023 04:30:20"
+        "download_time": "07-06-2023 05:30:21"
     },
     {
         "InterviewId": "2039",
