af49edd91c20f250c0f910193a96ece62bdd73ca
unknown <lb@silverliningresearch.com>
2023-06-14 01:30:53 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data_arr.js
+++ AUH_QSM_Dep/data/interview_data_arr.js
@@ -7,7 +7,7 @@ let interview_data_arr_raw = `[
         "Dest": "DOH",
         "AirlineCode": "QR",
         "Terminal": "T1",
-        "download_time": "14-06-2023 04:31:00"
+        "download_time": "14-06-2023 05:30:53"
     },
     {
         "InterviewId": "4226",
