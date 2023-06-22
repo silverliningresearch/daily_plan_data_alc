08d987dd980195fe4fcbd96dcb5fd3c6532052f2
unknown <lb@silverliningresearch.com>
2023-06-22 00:32:57 +0000

    update new data

--- BUD_Persona/data/interview_data.js
+++ BUD_Persona/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "22-06-2023 01:32:23"
+        "download_time": "22-06-2023 02:32:21"
     },
     {
         "InterviewId": "2039",
--- MUC_PEI_AB/data/interview_data_arr.js
+++ MUC_PEI_AB/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "AA - American Airlines",
         "Dest": "CLT",
         "AirlineCode": "AA",
-        "download_time": "22-06-2023 01:28:50"
+        "download_time": "22-06-2023 02:28:53"
     },
     {
         "InterviewId": "5174",
