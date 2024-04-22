9ec85440305f81ace6ae485e930f49782495d7ac
unknown <lb@silverliningresearch.com>
2024-04-22 00:34:00 +0000

    update new data

--- BUD_PaxFax/data/interview_data.js
+++ BUD_PaxFax/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "W6 2335",
         "Dest": "MXP",
         "AirlineCode": "W6",
-        "download_time": "22-04-2024 01:25:23"
+        "download_time": "22-04-2024 02:31:28"
     },
     {
         "InterviewId": "7701",
