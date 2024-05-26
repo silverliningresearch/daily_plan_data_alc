4981860b86783d9e4d809d67806f1ab3180207ab
unknown <lb@silverliningresearch.com>
2024-05-26 16:33:37 +0000

    update new data

--- AUH_QSM_Dep/data/interview_data.js
+++ AUH_QSM_Dep/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "SLL",
         "AirlineCode": "3L263",
         "Terminal": "T1",
-        "download_time": "26-05-2024 19:31:18"
+        "download_time": "26-05-2024 20:33:05"
     },
     {
         "InterviewId": "6722",
--- AUH_QSM_Dep/data/interview_data_arr.js
+++ AUH_QSM_Dep/data/interview_data_arr.js
@@ -7,7 +7,7 @@ let interview_data_arr_raw = `[
         "Dest": "BOM",
         "AirlineCode": "EY",
         "Terminal": "T3",
-        "download_time": "26-05-2024 19:31:49"
+        "download_time": "26-05-2024 20:33:36"
     },
     {
         "InterviewId": "3906",
