dc2cdd8705cfb14f7998d1a7d31c028ac01faa4d
unknown <lb@silverliningresearch.com>
2024-06-03 22:46:50 +0000

    update new data

--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E3/E4",
         "Gate": "G64",
-        "download_time": "03-06-2024 23:36:28"
+        "download_time": "04-06-2024 00:41:18"
     },
     {
         "aodbUniqueId": "20240603659946",
