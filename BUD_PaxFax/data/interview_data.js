46603e7bce39bbefd98261d0f926b07d1ad9a028
unknown <lb@silverliningresearch.com>
2024-07-09 23:13:40 +0000

    update new data

--- BUD_PaxFax/data/interview_data.js
+++ BUD_PaxFax/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "W6 2335",
         "Dest": "MXP",
         "AirlineCode": "W6",
-        "download_time": "10-07-2024 00:10:27"
+        "download_time": "10-07-2024 01:13:39"
     },
     {
         "InterviewId": "7701",
--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E3/E4",
         "Gate": "G47",
-        "download_time": "10-07-2024 00:04:50"
+        "download_time": "10-07-2024 01:09:46"
     },
     {
         "aodbUniqueId": "20240709770910",
