8d546da6fcb0573c95d37def957bbccfeb1b9844
unknown <lb@silverliningresearch.com>
2024-07-19 08:29:58 +0000

    update new data

--- MUC_PEI_AB/data/interview_data.js
+++ MUC_PEI_AB/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "AF 1823",
         "Dest": "CDG",
         "AirlineCode": "AF",
-        "download_time": "19-07-2024 09:24:49"
+        "download_time": "19-07-2024 10:29:51"
     },
     {
         "InterviewId": "15819",
