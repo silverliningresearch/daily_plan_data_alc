47e99b32fa49a663fd5f5171a0a8b426c69006ce
unknown <lb@silverliningresearch.com>
2024-08-10 00:35:31 +0000

    update new data

--- BUD_QSM_Dep/data/interview_data_arr.js
+++ BUD_QSM_Dep/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "W6 - Wizzair",
         "Dest": "TLV",
         "AirlineCode": "W6",
-        "download_time": "10-08-2024 01:27:48"
+        "download_time": "10-08-2024 02:32:50"
     },
     {
         "InterviewId": "1224",
