97bd32b5214b062c1786daca858522c1819f3ee0
unknown <lb@silverliningresearch.com>
2024-09-21 01:10:42 +0000

    update new data

--- BUD_PaxFax/data/interview_data.js
+++ BUD_PaxFax/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "W6 2335",
         "Dest": "MXP",
         "AirlineCode": "W6",
-        "download_time": "21-09-2024 02:10:57"
+        "download_time": "21-09-2024 03:10:40"
     },
     {
         "InterviewId": "7701",
--- MUC_PEI_AB/data/interview_data_arr.js
+++ MUC_PEI_AB/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "AA - American Airlines",
         "Dest": "CLT",
         "AirlineCode": "AA",
-        "download_time": "21-09-2024 02:02:05"
+        "download_time": "21-09-2024 03:06:25"
     },
     {
         "InterviewId": "5167",
