ac1ee9132cd0e82339a46c667ae8108bc9433899
unknown <lb@silverliningresearch.com>
2024-10-04 03:28:52 +0000

    update new data

--- BLL_QSM_Dep/data/interview_data.js
+++ BLL_QSM_Dep/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "KL1342",
         "Dest": "AMS",
         "AirlineCode": "KL1342",
-        "download_time": "04-10-2024 05:16:42"
+        "download_time": "04-10-2024 05:28:49"
     },
     {
         "InterviewId": "6093",
