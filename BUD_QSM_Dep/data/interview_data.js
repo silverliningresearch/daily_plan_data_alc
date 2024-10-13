c3eec9843b0471fae95829fc875fc3ad5f4e6b4d
unknown <lb@silverliningresearch.com>
2024-10-13 03:59:06 +0000

    update new data

--- BUD_QSM_Dep/data/interview_data.js
+++ BUD_QSM_Dep/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Flight": "EW  789",
         "Dest": "CGN",
         "AirlineCode": "EW",
-        "download_time": "13-10-2024 04:53:16"
+        "download_time": "13-10-2024 05:57:42"
     },
     {
         "InterviewId": "3965",
