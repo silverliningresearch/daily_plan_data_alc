7d7a0950990bfc4c826d445dc519e373b34f7ca6
unknown <lb@silverliningresearch.com>
2024-10-24 01:10:41 +0000

    update new data

--- BLL_QSM_Dep/data/interview_data_arr.js
+++ BLL_QSM_Dep/data/interview_data_arr.js
@@ -6,7 +6,7 @@ let interview_data_arr_raw = `[
         "Flight": "KL - KLM",
         "Dest": "AMS",
         "AirlineCode": "KL",
-        "download_time": "24-10-2024 01:08:03"
+        "download_time": "24-10-2024 03:07:34"
     },
     {
         "InterviewId": "2077",
