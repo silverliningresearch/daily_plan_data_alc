866ab74662f2d27e859281183d82e891ed201010
unknown <lb@silverliningresearch.com>
2025-01-05 23:10:10 +0000

    update new data

--- BUD_Route/data/interview_statistics.js
+++ BUD_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "LHR",
         "AirlineCode": "BA",
         "completed_interviews": 19,
-        "download_time": "05-01-2025 23:09:04"
+        "download_time": "06-01-2025 00:08:54"
     },
     {
         "Interview_Date": "2023-01-21",
--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E3/E4",
         "Gate": "G19",
-        "download_time": "05-01-2025 23:06:52"
+        "download_time": "06-01-2025 00:06:50"
     },
     {
         "aodbUniqueId": "20250105625399",
