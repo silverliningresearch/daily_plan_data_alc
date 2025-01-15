c18e3ebc44a493cfc48cd7db47bd3a1d9560f5ba
unknown <lb@silverliningresearch.com>
2025-01-15 22:11:17 +0000

    update new data

--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E3/E4",
         "Gate": "G32",
-        "download_time": "15-01-2025 22:07:23"
+        "download_time": "15-01-2025 23:07:06"
     },
     {
         "aodbUniqueId": "20250115649911",
