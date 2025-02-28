80b298bb2f3f94270c33247a1bc0e18cf974e0c8
unknown <lb@silverliningresearch.com>
2025-02-28 22:10:31 +0000

    update new data

--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Main E3/E4",
         "Gate": "G30",
-        "download_time": "28-02-2025 22:07:21"
+        "download_time": "28-02-2025 23:06:58"
     },
     {
         "aodbUniqueId": "20250228762580",
--- MUC_PEI_AB/data/interview_statistics.js
+++ MUC_PEI_AB/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CDG",
         "AirlineCode": "AF",
         "completed_interviews": 7,
-        "download_time": "28-02-2025 22:07:09"
+        "download_time": "28-02-2025 23:06:46"
     },
     {
         "Interview_Date": "2023-01-31",
