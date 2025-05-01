10ced4c551607676352a7ee0cde841db28539921
unknown <lb@silverliningresearch.com>
2025-05-01 23:14:40 +0000

    update new data

--- BUD_QSM_Dep/data/interview_statistics_arr.js
+++ BUD_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "CAI",
         "AirlineCode": "MS",
         "completed_interviews": 5,
-        "download_time": "02-05-2025 00:10:55"
+        "download_time": "02-05-2025 01:10:45"
     },
     {
         "Interview_Date": "2023-04-08",
--- MUC_PEI_AB/data/MUC_Gate_Info.js
+++ MUC_PEI_AB/data/MUC_Gate_Info.js
@@ -8,7 +8,7 @@ let MUC_Gate_Info = `[
         "terminalIdentifier": "2",
         "GateArea": "T2 Sat E4",
         "Gate": "K26",
-        "download_time": "02-05-2025 00:08:17"
+        "download_time": "02-05-2025 01:08:25"
     },
     {
         "aodbUniqueId": "20250501231444",
