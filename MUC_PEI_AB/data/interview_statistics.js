899d481347f4eaf296b1064fa19d7e6336d4f859
unknown <lb@silverliningresearch.com>
2025-08-02 00:10:10 +0000

    update new data

--- MUC_PEI_AB/data/interview_statistics.js
+++ MUC_PEI_AB/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CDG",
         "AirlineCode": "AF",
         "completed_interviews": 7,
-        "download_time": "02-08-2025 01:06:32"
+        "download_time": "02-08-2025 02:07:29"
     },
     {
         "Interview_Date": "2023-01-31",
