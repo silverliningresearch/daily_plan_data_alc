650f8170f7eb199e5fcc7fc5fa04a96ce9a0c68b
unknown <lb@silverliningresearch.com>
2025-08-31 00:09:11 +0000

    update new data

--- BUD_Route/data/interview_statistics.js
+++ BUD_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "LHR",
         "AirlineCode": "BA",
         "completed_interviews": 19,
-        "download_time": "31-08-2025 01:06:34"
+        "download_time": "31-08-2025 02:08:46"
     },
     {
         "Interview_Date": "2023-01-21",
