3f09087f959f48f613d704e5ff027fe921579bf7
unknown <lb@silverliningresearch.com>
2025-10-19 00:12:18 +0000

    update new data

--- BLL_QSM_Dep/data/interview_statistics.js
+++ BLL_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL1346",
         "completed_interviews": 5,
-        "download_time": "19-10-2025 01:07:43"
+        "download_time": "19-10-2025 02:08:52"
     },
     {
         "Interview_Date": "2023-02-01",
--- BUD_QSM_Dep/data/interview_statistics.js
+++ BUD_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CGN",
         "AirlineCode": "EW",
         "completed_interviews": 3,
-        "download_time": "19-10-2025 01:07:49"
+        "download_time": "19-10-2025 02:09:40"
     },
     {
         "Interview_Date": "2023-04-04",
