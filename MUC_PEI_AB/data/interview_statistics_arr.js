00249d128cc393116a968be7804cffde33621be2
unknown <lb@silverliningresearch.com>
2025-11-04 00:13:40 +0000

    update new data

--- MUC_PEI_AB/data/interview_statistics_arr.js
+++ MUC_PEI_AB/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "CGN",
         "AirlineCode": "EW",
         "completed_interviews": 3,
-        "download_time": "04-11-2025 00:06:56"
+        "download_time": "04-11-2025 01:08:51"
     },
     {
         "Interview_Date": "2023-01-31",
