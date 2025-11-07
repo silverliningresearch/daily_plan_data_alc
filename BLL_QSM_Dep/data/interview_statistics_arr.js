5008285c081529d647d64690afb847aa49dc011b
unknown <lb@silverliningresearch.com>
2025-11-07 00:12:31 +0000

    update new data

--- BLL_QSM_Dep/data/interview_statistics_arr.js
+++ BLL_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 5,
-        "download_time": "07-11-2025 00:08:23"
+        "download_time": "07-11-2025 01:08:20"
     },
     {
         "Interview_Date": "2023-02-01",
--- BUD_Route/data/interview_statistics.js
+++ BUD_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "LHR",
         "AirlineCode": "BA",
         "completed_interviews": 19,
-        "download_time": "07-11-2025 00:11:26"
+        "download_time": "07-11-2025 01:11:53"
     },
     {
         "Interview_Date": "2023-01-21",
