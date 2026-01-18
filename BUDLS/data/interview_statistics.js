43db46f93284d1f5e5a66c8c71602e52c6003423
unknown <lb@silverliningresearch.com>
2026-01-18 14:09:12 +0000

    update new data

--- BUDLS/data/interview_statistics.js
+++ BUDLS/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "EMPTY",
         "AirlineCode": "EMPTY",
         "completed_interviews": 1,
-        "download_time": "18-01-2026 14:06:22"
+        "download_time": "18-01-2026 15:08:29"
     },
     {
         "Interview_Date": "2025-12-04",
