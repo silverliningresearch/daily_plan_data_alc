b36c2a23c4d14a63fa8f68b44f781f7b14d064c1
unknown <lb@silverliningresearch.com>
2026-02-06 13:06:34 +0000

    update new data

--- BUD_QSM_Dep/data/interview_statistics_arr.js
+++ BUD_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "CAI",
         "AirlineCode": "MS",
         "completed_interviews": 5,
-        "download_time": "06-02-2026 13:07:58"
+        "download_time": "06-02-2026 14:06:12"
     },
     {
         "Interview_Date": "2023-04-08",
