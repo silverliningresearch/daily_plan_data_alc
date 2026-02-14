5deb80d0d43a3c144b05248adf889013cf98e85f
unknown <lb@silverliningresearch.com>
2026-02-14 20:09:25 +0000

    update new data

--- BLL_QSM_Dep/data/interview_statistics_arr.js
+++ BLL_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 5,
-        "download_time": "14-02-2026 20:06:17"
+        "download_time": "14-02-2026 21:07:58"
     },
     {
         "Interview_Date": "2023-02-01",
