e18f1e94d7be753c303365e01fb799d0a87090b4
unknown <lb@silverliningresearch.com>
2026-02-27 15:06:46 +0000

    update new data

--- BUD_QSM_Dep/data/interview_statistics.js
+++ BUD_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CGN",
         "AirlineCode": "EW",
         "completed_interviews": 3,
-        "download_time": "27-02-2026 15:06:46"
+        "download_time": "27-02-2026 16:06:19"
     },
     {
         "Interview_Date": "2023-04-04",
