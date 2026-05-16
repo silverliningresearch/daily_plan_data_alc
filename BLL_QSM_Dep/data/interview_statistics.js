274cd0389de8d0fefb46781f5d6f518f8e97d7a2
unknown <lb@silverliningresearch.com>
2026-05-16 18:10:10 +0000

    update new data

--- BLL_QSM_Dep/data/interview_statistics.js
+++ BLL_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL1346",
         "completed_interviews": 5,
-        "download_time": "16-05-2026 19:06:08"
+        "download_time": "16-05-2026 20:07:35"
     },
     {
         "Interview_Date": "2023-02-01",
@@ -18862,7 +18862,7 @@ let interview_statistics = `[
         "Interview_Date": "2026-05-16",
         "Dest": "BCN",
         "AirlineCode": "D85519",
-        "completed_interviews": 4
+        "completed_interviews": 5
     },
     {
         "Interview_Date": "2026-05-16",
--- BUD_PaxFax/data/interview_statistics.js
+++ BUD_PaxFax/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "FRA",
         "AirlineCode": "LH",
         "completed_interviews": 18,
-        "download_time": "16-05-2026 19:06:21"
+        "download_time": "16-05-2026 20:08:44"
     },
     {
         "Interview_Date": "2023-03-29",
@@ -21064,6 +21064,12 @@ let interview_statistics = `[
         "Interview_Date": "2026-05-16",
         "Dest": "MUC",
         "AirlineCode": "LH",
+        "completed_interviews": 4
+    },
+    {
+        "Interview_Date": "2026-05-16",
+        "Dest": "STN",
+        "AirlineCode": "FR",
         "completed_interviews": 4
     },
     {
