5cb63415a3d12d39a1ba1474d7222163bf42545c
unknown <lb@silverliningresearch.com>
2026-07-26 15:10:44 +0000

    update new data

--- BLL_QSM_Dep/data/interview_statistics.js
+++ BLL_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL1346",
         "completed_interviews": 5,
-        "download_time": "26-07-2026 16:09:57"
+        "download_time": "26-07-2026 17:10:09"
     },
     {
         "Interview_Date": "2023-02-01",
@@ -19762,13 +19762,25 @@ let interview_statistics = `[
         "Interview_Date": "2026-07-26",
         "Dest": "OSL",
         "AirlineCode": "DY995",
-        "completed_interviews": 1
+        "completed_interviews": 10
+    },
+    {
+        "Interview_Date": "2026-07-26",
+        "Dest": "RHO",
+        "AirlineCode": "DK1276",
+        "completed_interviews": 2
     },
     {
         "Interview_Date": "2026-07-26",
         "Dest": "RHO",
         "AirlineCode": "RC7146",
         "completed_interviews": 3
+    },
+    {
+        "Interview_Date": "2026-07-26",
+        "Dest": "TLL",
+        "AirlineCode": "BT814",
+        "completed_interviews": 1
     }
 ]    
 
--- BUD_QSM_Dep/data/interview_statistics.js
+++ BUD_QSM_Dep/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "CGN",
         "AirlineCode": "EW",
         "completed_interviews": 3,
-        "download_time": "26-07-2026 16:10:04"
+        "download_time": "26-07-2026 17:10:33"
     },
     {
         "Interview_Date": "2023-04-04",
