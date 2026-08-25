0ecc2c1c898f4d9fcd1f8d4b2b23776d41b17c8d
unknown <lb@silverliningresearch.com>
2026-08-25 15:11:40 +0000

    update new data

--- BUD_PaxFax/data/interview_statistics.js
+++ BUD_PaxFax/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "FRA",
         "AirlineCode": "LH",
         "completed_interviews": 18,
-        "download_time": "25-08-2026 16:10:59"
+        "download_time": "25-08-2026 17:11:14"
     },
     {
         "Interview_Date": "2023-03-29",
@@ -23380,7 +23380,7 @@ let interview_statistics = `[
         "Interview_Date": "2026-08-25",
         "Dest": "ALC",
         "AirlineCode": "FR",
-        "completed_interviews": 3
+        "completed_interviews": 4
     },
     {
         "Interview_Date": "2026-08-25",
@@ -23392,6 +23392,12 @@ let interview_statistics = `[
         "Interview_Date": "2026-08-25",
         "Dest": "CAI",
         "AirlineCode": "MS",
+        "completed_interviews": 2
+    },
+    {
+        "Interview_Date": "2026-08-25",
+        "Dest": "FRA",
+        "AirlineCode": "DE",
         "completed_interviews": 2
     },
     {
--- BUD_QSM_Dep/data/interview_statistics_arr.js
+++ BUD_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "CAI",
         "AirlineCode": "MS",
         "completed_interviews": 5,
-        "download_time": "25-08-2026 16:10:37"
+        "download_time": "25-08-2026 17:10:53"
     },
     {
         "Interview_Date": "2023-04-08",
