ecf461837757c43bea6071f97b0d6fa8f3c0bf41
unknown <lb@silverliningresearch.com>
2026-09-26 21:50:24 +0000

    update new data

--- BUD_PaxFax/data/interview_statistics.js
+++ BUD_PaxFax/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "FRA",
         "AirlineCode": "LH",
         "completed_interviews": 18,
-        "download_time": "26-09-2026 22:49:00"
+        "download_time": "26-09-2026 23:50:10"
     },
     {
         "Interview_Date": "2023-03-29",
--- BUD_QSM_Dep/data/interview_statistics_arr.js
+++ BUD_QSM_Dep/data/interview_statistics_arr.js
@@ -4,7 +4,7 @@ let interview_statistics_arr = `[
         "Dest": "CAI",
         "AirlineCode": "MS",
         "completed_interviews": 5,
-        "download_time": "26-09-2026 22:48:44"
+        "download_time": "26-09-2026 23:49:49"
     },
     {
         "Interview_Date": "2023-04-08",
